93da225b29b2b53a33b17e89093016bd44400fe0
Johnny <joaogardenberg@gmail.com>
2023-05-11 13:19:58 +0200

    Initial Shallow

---
+++ src/components/home/Shallow.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const StyledShallow = styled.div`
+  background-color: #0782a6;
+  min-height: 100vh;
+`
+
+export default function Shallow() {
+  return <StyledShallow />
+}
